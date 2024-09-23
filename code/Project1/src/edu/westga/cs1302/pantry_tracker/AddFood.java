96023980796ce5d04248f8396a79bdf61ee7e5e6
Justice L Ricks <jr00381@my.westga.edu>
2024-09-23 18:38:33 -0400

    Added New Food to Pantry
    i created a AddFood class that will layout the name of the food and the type of food in the pantry i also made a gui to list where you select the type of food in the pantry and where you can name the food we did this for stability and readabilty so that for a overall ore organized project

---
+++ code/Project1/src/edu/westga/cs1302/pantry_tracker/AddFood.java
@@ -0,0 +1,13 @@
+package edu.westga.cs1302.pantry_tracker;
+
+/**
+ * a class that adds food new food items to the pantry
+ * 
+ * 
+ * @author Justice Ricks
+ * @version Fall 2024
+ * 
+ */
+public class AddFood {
+
+}
